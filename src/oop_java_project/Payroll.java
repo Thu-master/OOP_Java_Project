6843baf97466d4e78ef7d9a83ff4eb741cd9a5c5
Thành_Thu <thu.vt00790@sinhvien.hoasen.edu.vn>
2024-11-19 11:11:27 +0700

    class Payrol Ý tưởng cho tính lương nhân viên với quyền manager

---
+++ src/oop_java_project/Payroll.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop_java_project;
+
+/**
+ *
+ * @author Thu
+ */
+public class Payroll 
+{
+    // ý tưởng là tính lương nhân viên với quyền hạn manager, tính lương dùng abstraction
+    
+}
