d2f224d7db2dd6ff69dd2c845678b9229f44d882
Thu-master <thuvoong@gmail.com>
2024-11-24 02:43:13 +0700

    Khai báo lớp cha Product
    Contructor và các tham số truyền vào

--- src/Temp_Class/Product.java
+++ src/Temp_Class/Product.java
@@ -10,5 +10,46 @@ package Temp_Class;
  */
 public class Product 
 {
-//Đây là lớp cha chung cho các loại mặt hàng.    
+//Đây là lớp cha chung cho các loại mặt hàng.  
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+    
+    public Product()
+    {
+        
+    }
+    
+    public Product(String id, String name, double price, int quantity) 
+    {
+       this.id = id;
+       this.name = name;
+       this.price = price;
+       this.quantity = quantity;
+    }
+
+    // Getters và setters
+    public String getId() 
+    { 
+       return id; 
+    }
+    public String getName() 
+    { 
+       return name; 
+    }
+    public double getPrice() 
+    { 
+       return price; 
+    }
+    public int getQuantity() 
+    { 
+       return quantity; 
+    }
+    
+    public void displayInfo() 
+    {
+       System.out.println("ID: " + id + ", Name: " + name + ", Price: " + price + ", Quantity: " + quantity);
+    }
+    
 }
