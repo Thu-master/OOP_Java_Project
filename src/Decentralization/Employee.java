d8209cc256420312981b969cbf5dc80b5431eab1
Thu-master <thuvoong@gmail.com>
2024-11-24 02:49:00 +0700

    Đổi packages employee

--- src/Decentralization/Employee.java
+++ src/Decentralization/Employee.java
@@ -2,16 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Temp_Class;
+package Decentralization;
 
 /**
  *
  * @author Thu
  */
-public class Employee 
+public class Employee extends User
 {
     //để quản lý thông tin nhân viên, như ID, tên, chức vụ (nhân viên bán hàng, quản lý, thủ kho), và thông tin liên lạc.
     //Phân quyền nhân viên: Tùy thuộc vào chức vụ, bạn có thể giới hạn quyền truy cập để chỉ cho phép một số nhân viên nhất định thực hiện các thao tác như nhập hàng, quản lý đơn hàng, hoặc xem báo cáo.
-    int sdt, msnv;
-    String name;
+    public Employee(String employeeId, String fullname, String role) 
+    {
+        super(employeeId, fullname, role);
+    }
+
+    @Override
+    public void showPermissions() 
+    {
+       System.out.println("Quyen han: Them, xoa, sua sach.");
+    }
 }
