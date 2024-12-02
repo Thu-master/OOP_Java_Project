e2bc260b13fdde452833200cbdc9376b140ceec6
mquan <quan.tnm01525@sinhvien.hoasen.edu.vn>
2024-12-03 02:25:47 +0700

    thêm packages hóa đơn, thêm hàm getTotalPrice và hàm toString vào class Product

---
+++ src/Invoice/Bill.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Invoice;
+
+import Funtion.Product;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Windows 10 Pro Nvme
+ */
+public class Bill {
+    String customer;
+    ArrayList<Product> products = new ArrayList<>();
+
+    public Bill (String customer) {
+        this.customer = customer;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void printInvoice() {
+        System.out.println("===============================================");
+        System.out.println("           HOA DON BAN HANG            ");
+        System.out.println("===============================================");
+        System.out.println("Khach hang: " + customer);
+        System.out.println("-----------------------------------------------");
+        System.out.println("ID\tten san pham\tDon gia\tSL\tThanh tien");
+        System.out.println("-----------------------------------------------");
+        double total = 0;
+        for (int i = 0; i < products.size();i++) 
+        {
+        Product product = products.get(i);
+        System.out.println(product);
+        total += product.getTotalPrice();
+    }
+        
+        System.out.println("-----------------------------------------------");
+        System.out.printf("%-20s %26.2f\n", "Tong cong:", total);
+        System.out.println("===============================================");
+}
+}
